34a61cc55e65bb03c79b368c992370c741d0550e
vizart-vj <62356719+vizart-vj@users.noreply.github.com>
2026-08-20 01:55:07 +0300

    Delete __init__ (3).py

--- __init__ (3).py
+++
@@ -1,8 +0,0 @@
-__version__ = "0.4.2"
-
-from . import lora_compat as _lora_compat  # noqa: F401
-from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-
-WEB_DIRECTORY = "./web"
-
-__all__ = ["__version__", "NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
